bb4320f8e16bb2cf0f3004c857b0f0920eb90b96
SergeyR <sergey17232@mail.ru>
2025-09-18 16:47:57 +0300

    add OpenAPI documentation with drf-spectacular - Install drf-spectecular package for OpenAPI documentaion - Configured settings.py for package - Add basic API views

--- backend/todo_project/urls.py
+++ backend/todo_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from todo_app.views import TaskViewSet
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
@@ -9,4 +10,7 @@ router.register(r'tasks', TaskViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(), name='swagger-ui')
 ]
